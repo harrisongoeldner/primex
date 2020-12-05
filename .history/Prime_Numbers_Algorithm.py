dd6458d812ef6dbd731542f237b3c2a88955700e
harrisongoeldner <harrisonjvg@gmail.com>
2020-12-05 10:08:25 -0800

    File structure reorganization

--- .history/Prime_Numbers_Algorithm.py
+++ .history/Prime_Numbers_Algorithm.py
@@ -1,8 +1,7 @@
 # A optimized school method based  
 # Python3 program to check 
 # if a number is prime 
-
-#import sys  
+  
   
 def isPrime(n) : 
   
@@ -25,48 +24,27 @@ def isPrime(n) :
   
     return True
 
-print("""
--- Prime Number Algorithm --
-Version... 1.1.3
-Release Date... April 28, 2020
-Source code by Nikita Tiwari
-Developed by Harrison Goeldner
-""")
-
 num = int(input("bottom value: "))
 num1 = int(input("top value: "))
-diff = num1 - num
 list = []
 
 # Create txt file to store data
-t = 1 # used to name file -- could be used in future in a loop
-f= open("prime"+str(num)+'-'+str(num1)+ ".txt","w+")
+t = str(num)+'-'+str(num1) # used to name file
+f= open("prime"+str(t)+".txt","w+")
 
 # Runs function
 for x in range(num,num1):
-    percent = str(round(x/diff*100,1))
     if(isPrime(num)):
         list.append(num)
-        print(str(num) + ' prime'.rjust(20,'-'), end = '')
-        #alt could use sys.stdout.write()
+        print(str(num) + ' prime')
     else:
-        print(str(num) + ' null-'.rjust(20,'-'), end = '')
-    print(percent.rjust(20,'-')+'%')
+        print(str(num) + ' null')
     num = num + 1
       
 print(list)
-print('collected '+str(len(list))+' prime numbers')
-f.write(str(list)+'\n\ncollected '+str(len(list))+' prime numbers')
+f.write(str(list))
 f.close() 
 
 # Coded by Harrison Goeldner
 # This code is contributed  
 # by Nikita Tiwari.
-
-
-"""
--- UPDATE LOG --
-22 APRIL 2020    1.1.3 add custom file name
-18 APRIL 2020    1.1.2 add oercent and improved interface
-00 UNKNOWN 0000   1.1.1 original version
-"""
--- Prime_Numbers_Algorithm.py
+++ Prime_Numbers_Algorithm.py
@@ -1,7 +1,8 @@
 # A optimized school method based  
 # Python3 program to check 
 # if a number is prime 
-  
+
+#import sys  
   
 def isPrime(n) : 
   
@@ -24,27 +25,48 @@ def isPrime(n) :
   
     return True
 
+print("""
+-- Prime Number Algorithm --
+Version... 1.1.3
+Release Date... April 28, 2020
+Source code by Nikita Tiwari
+Developed by Harrison Goeldner
+""")
+
 num = int(input("bottom value: "))
 num1 = int(input("top value: "))
+diff = num1 - num
 list = []
 
 # Create txt file to store data
-t = str(num)+'-'+str(num1) # used to name file
-f= open("prime"+str(t)+".txt","w+")
+t = 1 # used to name file -- could be used in future in a loop
+f= open("prime"+str(num)+'-'+str(num1)+ ".txt","w+")
 
 # Runs function
 for x in range(num,num1):
+    percent = str(round(x/diff*100,1))
     if(isPrime(num)):
         list.append(num)
-        print(str(num) + ' prime')
+        print(str(num) + ' prime'.rjust(20,'-'), end = '')
+        #alt could use sys.stdout.write()
     else:
-        print(str(num) + ' null')
+        print(str(num) + ' null-'.rjust(20,'-'), end = '')
+    print(percent.rjust(20,'-')+'%')
     num = num + 1
       
 print(list)
-f.write(str(list))
+print('collected '+str(len(list))+' prime numbers')
+f.write(str(list)+'\n\ncollected '+str(len(list))+' prime numbers')
 f.close() 
 
 # Coded by Harrison Goeldner
 # This code is contributed  
 # by Nikita Tiwari.
+
+
+"""
+-- UPDATE LOG --
+22 APRIL 2020    1.1.3 add custom file name
+18 APRIL 2020    1.1.2 add oercent and improved interface
+00 UNKNOWN 0000   1.1.1 original version
+"""
